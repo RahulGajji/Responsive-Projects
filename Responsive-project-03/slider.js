38996f7071481e821bf6d77e9de6059d5cad7105
RahulGajji <rahulgajjig@gmail.com>
2024-09-26 13:43:32 +0530

    commit changes

---
+++ Responsive-project-03/slider.js
@@ -0,0 +1,19 @@
+function leftscroll() {
+
+  document.getElementById('varityslider').scrollBy({
+    left: -300,
+    behavior:"smooth"
+  });
+
+ }
+
+
+ 
+ function rightscroll() {
+
+document.getElementById('varityslider').scrollBy({
+  left: 300,
+  behavior:"smooth"
+});
+
+}
